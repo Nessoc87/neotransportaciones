41514cc0d903e84766bcb588737a5a7eb6294f14
Nessoc87 <nessoc87@gmail.com>
2026-08-15 15:24:44 -0600

    boton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,14 +18,14 @@ export default function Hero() {
             maniobras y carga general con cobertura en todo México.
           </p>
 
-          <div className="mt-10 flex flex-wrap gap-4">
+          {/* <div className="mt-10 flex flex-wrap gap-4">
             <button className="bg-orange-500 hover:bg-orange-600 px-6 py-3 rounded-lg font-semibold transition">
               Solicitar Cotización
-            </button>
+            </button> */}
 
-            <button className="border border-white hover:bg-white hover:text-slate-900 px-6 py-3 rounded-lg transition">
+            <button className="bg-orange-500 hover:bg-orange-600 px-6 py-3 rounded-lg font-semibold transition">
               Contactar
-            </button>
+            </button> 
           </div>
         </div>
 
